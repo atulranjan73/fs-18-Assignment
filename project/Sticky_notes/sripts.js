022fd5ac5be6f1b97db77023e5ce245f792821f4
AtulRanjan <atulranjan164@gmail.com>
2024-04-20 19:45:05 +0530

    complete notes project

--- project/Sticky_notes/sripts.js
+++ project/Sticky_notes/sripts.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const addButton = document.getElementById("btn-1");
     const textarea = document.getElementById("textarea");
     const colorPicker = document.getElementById("color");
-    const outputContainer = document.querySelector(".right");
+    const outputContainer = document.querySelector(".notes-container");
 
     addButton.addEventListener("click", function () {
         const noteText = textarea.value.trim();

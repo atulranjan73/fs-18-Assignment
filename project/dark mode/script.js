22138e5f646c1b03a0065abc0562dfb07675f956
AtulRanjan <atulranjan164@gmail.com>
2024-04-21 10:06:56 +0530

    add dark mood project

--- project/dark mode/script.js
+++ project/dark mode/script.js
@@ -1,13 +1,16 @@
 const togglebtn = document.querySelector("#checkbox");
+const heading =document.querySelector(".heading");
 
 togglebtn.addEventListener('click' , ()=>{
    if(togglebtn.checked){
     document.body.style.backgroundColor="black";
     document.getElementById("toggle").style.backgroundColor="grey";
+    heading.style.color = " white"
    }
    else{
     document.body.style.backgroundColor="white";
     document.getElementById("toggle").style.backgroundColor="black";
+    heading.style.color = "black"
 
 
    }

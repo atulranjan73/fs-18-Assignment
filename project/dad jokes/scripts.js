4bac2eeacc2393572bb4830af7af54987cd87ed3
AtulRanjan <atulranjan164@gmail.com>
2024-05-01 21:29:17 +0530

    add

---
+++ project/dad jokes/scripts.js
@@ -0,0 +1,23 @@
+
+
+const jokePara = document.getElementById("jokePara");
+
+async function getDataFromAPI() {
+    let config = {
+        headers:{
+            Accept : "application/json",
+        },
+    };
+    let a = await fetch("https://icanhazdadjoke.com" , config)
+    let b = await a.json();
+    console.log(b.joke);
+    jokePara.innerText = b.joke+"🤪";
+
+}
+getDataFromAPI();
+
+document.getElementById("jokeBtn").addEventListener( ('click') , getDataFromAPI);
+
+
+
+// fetch("https://api.api-ninjas.com/v1/dadjokes")

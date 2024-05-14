0c5ce8cd52f8633ac6aa54e21fe1a127d6268277
AtulRanjan <atulranjan164@gmail.com>
2024-05-15 01:37:23 +0530

    add

--- project/u mater/home/scripts.js
+++ project/u mater/home/scripts.js
@@ -20,3 +20,13 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+
+    const navbar = document.getElementById('nav');
+    const toggleButton = document.getElementById('toggle-button');
+
+    toggleButton.addEventListener('click', () => {
+        navbar.classList.toggle('acti');
+    });
+

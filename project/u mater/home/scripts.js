c6cd89af4cdb1f2f17b004dadaa687b45d91d83c
AtulRanjan <atulranjan164@gmail.com>
2024-05-16 14:54:13 +0530

    add

--- project/u mater/home/scripts.js
+++ project/u mater/home/scripts.js
@@ -27,3 +27,18 @@ const toggleButton = document.getElementById("toggle-button");
 toggleButton.addEventListener("click", () => {
   navbar.classList.toggle("acti");
 });
+
+
+
+
+  const phrases = ["Are you strong?", "You are strong!", "I love you","You did great today!" ,"Be a light." ,"Be good. Do good" ,"Stay hopeful!" ,"Protect your peace" ,"Keep on fighting!" ,"I believe in you!"];
+  let index = 0;
+
+  function changeText() {
+    const spanElement = document.getElementById("dynamic-text");
+    index = (index + 1) % phrases.length;
+    spanElement.textContent = phrases[index];
+  }
+
+  setInterval(changeText, 1000);
+

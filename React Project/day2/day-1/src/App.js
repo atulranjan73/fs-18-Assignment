724e1af1f1dfde8f1ee43a0f34a868942d40b638
AtulRanjan <atulranjan164@gmail.com>
2024-05-25 15:06:49 +0530

    add

---
+++ React Project/day2/day-1/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Learning Web Development</h1>
+      <p>
+        Welcome to the MDN learning area. This set of articles aims to provide complete beginners to web development with fundamental skills for coding websites. The aim is not to take you from "beginner" to "expert" but to take you from "beginner" to "comfortable." From there, you should be able to start making your way, learning from <a href="https://developer.mozilla.org/en-US/docs/Learn">the rest of MDN</a>, and other intermediate to advanced resources that assume a lot of previous knowledge.
+      </p>
+      <p>
+        If you are a complete beginner, web development can be challenging — we will hold your hand and provide enough detail for you to feel comfortable and learn the topics properly. You should feel at home whether you are a student learning web development (on your own or as part of a class), a teacher looking for class materials, a hobbyist, or someone who just wants to understand more about how web technologies work.
+      </p>
+    </div>
+  );
+}
+
+export default App;

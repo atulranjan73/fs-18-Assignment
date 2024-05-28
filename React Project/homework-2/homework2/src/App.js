bf3fc3b8566ee3631be796478591aa4d1d19a571
AtulRanjan <atulranjan164@gmail.com>
2024-05-28 22:12:08 +0530

    add

---
+++ React Project/homework-2/homework2/src/App.js
@@ -0,0 +1,10 @@
+import Main from './Main';
+import './App.css';
+
+function App() {
+  return (
+  <Main/>
+  );
+}
+
+export default App;
---
+++ React Project/homework-2/homework2/src/Main.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Main() {
+  return (
+    <div className='container'>
+   <div className='main'>
+   <h1 className='heading'>Topic Coverd</h1>
+     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae</p>
+     <div>
+      <a href="https://drive.google.com/file/d/16oGHiOaCzMW7lpDmwQ94BaZvLpmQLike/view">Getting started with webdevlopment</a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, quod minus earum vitae voluptas tempore perferendis aut voluptates! Praes</p>
+     </div>
+     <div>
+      <a href="https://drive.google.com/file/d/16oGHiOaCzMW7lpDmwQ94BaZvLpmQLike/view">HTML- Structuring the web</a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, quod minus earum vitae voluptas tempore perferendis aut voluptates! Praes</p>
+     </div>
+     <div>
+      <a href="https://drive.google.com/file/d/16oGHiOaCzMW7lpDmwQ94BaZvLpmQLike/view">Css Styling the web </a>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, quod minus earum vitae voluptas tempore perferendis aut voluptates! Praes</p>
+     </div>
+   </div>
+    </div>
+  )
+}

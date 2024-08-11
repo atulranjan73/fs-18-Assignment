eb937cb41737f0203540ec064350e107939d432f
AtulRanjan <atulranjan164@gmail.com>
2024-08-11 23:49:35 +0530

    add

--- project/color and shape random/random.js
+++ project/color and shape random/random.js
@@ -5,8 +5,23 @@ const headingColor = document.getElementById("heading");
 
 const shapes = [
     'circle(50% at 50% 50%)', // Circle
-    'polygon(50% 0%, 0% 100%, 100% 100%)', // Triangle
-    'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)' // Rectangle
+'polygon(50% 0%, 0% 100%, 100% 100%)', // Triangle
+'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', // Rectangle
+'ellipse(50% 40% at 50% 50%)', // Ellipse
+'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)', // Diamond
+'polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%)', // Pentagon
+'polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)', // Hexagon
+'polygon(30% 0%, 70% 0%, 100% 30%, 100% 70%, 70% 100%, 30% 100%, 0% 70%, 0% 30%)', // Octagon
+'polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)', // Star
+'polygon(25% 0%, 100% 0%, 75% 100%, 0% 100%)', // Parallelogram
+'polygon(25% 0%, 75% 0%, 100% 100%, 0% 100%)', // Trapezoid
+'ellipse(50% 50% at 50% 50%)', // Crescent
+'polygon(50% 0%, 100% 50%, 75% 50%, 75% 100%, 25% 100%, 25% 50%, 0% 50%)', // Arrow
+'polygon(50% 0%, 75% 0%, 100% 25%, 50% 100%, 0% 25%, 25% 0%)', // Heart
+'polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%)', // Rhombus
+'polygon(0% 0%, 100% 0%, 0% 100%)', // Right Triangle
+// 'polygon(50% 0%, 
+
 ];
 
 let currentShapeIndex = 0;
--- project/dad jokes/scripts.js
+++ project/dad jokes/scripts.js
@@ -1,20 +1,37 @@
 
 
-const jokePara = document.getElementById("jokePara");
+// const jokePara = document.getElementById("jokePara");
 
-async function getDataFromAPI() {
+// async function getDataFromAPI() {
+//     let config = {
+//         headers:{
+//             Accept : "application/json",
+//         },
+//     };
+//     let a = await fetch("https://icanhazdadjoke.com" , config)
+//     let b = await a.json();
+//     console.log(b.joke);
+//     jokePara.innerText = b.joke+"🤪";
+
+// }
+// getDataFromAPI();
+
+// document.getElementById("jokeBtn").addEventListener( ('click') , getDataFromAPI);
+
+const jokepara = document.getElementById("jokepara");
+
+
+async function getdata(){
     let config = {
-        headers:{
-            Accept : "application/json",
-        },
-    };
-    let a = await fetch("https://icanhazdadjoke.com" , config)
-    let b = await a.json();
-    console.log(b.joke);
-    jokePara.innerText = b.joke+"🤪";
-
+              headers:{
+                     Accept : "application/json",
+                },
+};
+let a = await fetch("https://icanhazdadjoke.com" , config)
+let b = await a.json();
+console.log(b.joke);
+jokepara.innerText = b.joke 
 }
-getDataFromAPI();
 
-document.getElementById("jokeBtn").addEventListener( ('click') , getDataFromAPI);
-
+const btn  = document.getElementById("jokeBtn");
+btn.addEventListener('click' , getdata)
